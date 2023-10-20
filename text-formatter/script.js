c2149a5bce0415c027333406e68c8a8971d86e32
Samantha19 <samantha.monico4@gmail.com>
2023-10-20 10:07:33 -0400

    more work

--- text-formatter/script.js
+++ text-formatter/script.js
@@ -33,14 +33,50 @@ const pictureOutput = document.getElementById("pictureoutput");
 
 function changeText() {
     const text = textInput.value;
-    pictureOutput.innerText = formatText(text);
+    pictureOutput.innerHTML = formatText(text);
 
 }
 textInput.addEventListener("input", changeText);
 
+
+function getFontFromComputedStyle (computedStyle) {
+    let font = computedStyle.font;
+    // Firefox returns the empty string for .font, so create the .font property manually
+    if (font === '') {
+      // Firefox uses percentages for font-stretch, but Canvas does not accept percentages
+      // so convert to keywords, as listed at:
+      //   https://developer.mozilla.org/en-US/docs/Web/CSS/font-stretch
+      let fontStretchLookupTable = {
+        '50%': 'ultra-condensed',
+        '62.5%': 'extra-condensed',
+        '75%': 'condensed',
+        '87.5%': 'semi-condensed',
+        '100%': 'normal',
+        '112.5%': 'semi-expanded',
+        '125%': 'expanded',
+        '150%': 'extra-expanded',
+        '200%': 'ultra-expanded'
+      };
+      // If the retrieved font-stretch percentage isn't found in the lookup table, use
+      // 'normal' as a last resort.
+      let fontStretch = fontStretchLookupTable.hasOwnProperty(computedStyle.fontStretch)
+        ? fontStretchLookupTable[computedStyle.fontStretch]
+        : 'normal';
+      font = computedStyle.fontStyle
+        + ' ' + computedStyle.fontVariant
+        + ' ' + computedStyle.fontWeight
+        + ' ' + fontStretch
+        + ' ' + computedStyle.fontSize
+        + '/' + computedStyle.lineHeight
+        + ' ' + computedStyle.fontFamily;
+    }
+    return font;
+  }
+
+
 function getTextWidth(inputText) { 
-    const font = document.getElementById("pictureoutput").style.font;
- 
+    const output = document.getElementById("pictureoutput");
+    const font = getComputedStyle(output).font;
     canvas = document.createElement("canvas"); 
     context = canvas.getContext("2d"); 
     context.font = font; 
@@ -72,23 +108,27 @@ function formatText(text) {
             if(lineBoundaries.inBounds(getTextWidth(line) / outputWidth)) {
                 line.push(text[index]);
                 index ++;
-                continue
+                continue;
             }
-            line.push("_");
+            line.push("&nbsp;");
         }
 
             //if we're on a line
             
-        line.push('\n')
+        line.push('<br>')
         newText.push(...line);
 
     }
+    console.log(newText)
     console.log(newText.join(''))
     return newText.join('');
 
 }
 
 console.log(getTextWidth("a"))
-console.log(getTextWidth("aa"))
-console.log(getTextWidth("WWW"))
+console.log(getTextWidth("b"))
+console.log(getTextWidth("W"))
+console.log(getTextWidth("|"))
+console.log(getTextWidth("_"))
+const out = document.getElementById("pictureoutput");
 console.log(pictureOutput.offsetWidth)
