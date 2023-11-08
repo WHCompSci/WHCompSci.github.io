8a9d9f0d1a795b8b3ca8930c2d4927819c406289
Samantha19 <samantha.monico4@gmail.com>
2023-11-08 09:03:28 -0500

    new changes

--- schooldle/script.js
+++ schooldle/script.js
@@ -1,27 +1,27 @@
 const collegeDropDown = document.getElementById("colleges");
 
 const option = document.createElement("option");
+const guessTable = document.getElementById("guess-table");
 // option.value = weightClass.weight;
 // option.text = weightClass.label;
 // weightSelect.appendChild(option);
 // collegeDropDown.appendChild()
 
-collegeNames = []
-const $output = document.getElementById('output')
-document.getElementById('file').onchange = function() {
+const collegeNames = [];
+function readCollegeNames() {
   var file = this.files[0];
 
   var reader = new FileReader();
-  reader.onload = function(progressEvent) {
+  reader.onload = function (progressEvent) {
     // Entire file
     const text = this.result;
-    $output.innerText = text
+    $output.innerText = text;
 
     // By lines
     var lines = text.split('\n');
     for (var line = 0; line < lines.length; line++) {
       console.log(lines[line]);
-      collegeNames()
+      collegeNames();
     }
   };
   reader.readAsText(file);
