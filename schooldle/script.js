b8cde28e31b4d9647ebf322e7e3bf1fd46b038b5
skiddmarc <84139731+skiddmarc@users.noreply.github.com>
2023-11-08 22:39:36 -0500

    Lots of changes

--- schooldle/script.js
+++ schooldle/script.js
@@ -1,67 +1,67 @@
 const collegeDropDown = document.getElementById("colleges");
-
-const option = document.createElement("option");
+const collegeTextInput = document.getElementById("text-input");
 const guessTable = document.getElementById("guess-table");
+const guessButton = document.getElementById("guess-button");
 const urlOfFile = "collegelist.csv";
 // option.value = weightClass.weight;
 // option.text = weightClass.label;
 // weightSelect.appendChild(option);
 // collegeDropDown.appendChild()
 
-const collegeNames = [];
-function readCollegeNames() {
-  var file = this.files[0];
-
-  var reader = new FileReader();
-  reader.onload = function (progressEvent) {
-    // Entire file
-    const text = this.result;
-    $output.innerText = text;
-
-    // By lines
-    var lines = text.split('\n');
-    for (var line = 0; line < lines.length; line++) {
-      console.log(lines[line]);
-      collegeNames();
+const collegeData = [];
+const columnNames = [
+    "School",
+    "State",
+    "Control",
+    "Total Applications",
+    "Total Accepted",
+    "Acceptance Rate",
+    "Total Cost",
+    "Total Population",
+    "Male Population",
+    "Female Population",
+    "Male Percentage",
+    "Female Percentage",
+    "M/F",
+];
+// Wait for the DOM to load
+document.addEventListener("DOMContentLoaded", () => {
+    // Fetch the file
+    fetch("collegedata.csv")
+        .then((response) => response.text())
+        .then((content) => {
+            const lines = content.split("\n");
+            lines.forEach((line) => {
+                line = line.trim();
+                const firstComma = line.indexOf(",");
+                const firstWord = line.substring(0, firstComma);
+                if (firstWord) {
+                    collegeData.push(firstWord);
+                    const option = document.createElement("option");
+                    option.text = firstWord;
+                    option.value = firstWord;
+                    collegeDropDown.appendChild(option);
+                }
+            });
+            console.log(collegeData);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+});
+//function to handle the college guess.
+guessButton.addEventListener("click", () => {
+    const selectedSchool = collegeTextInput.value;
+    if (selectedSchool === "") return
+    console.log("Guessing the School: \""+selectedSchool+"\"");
+});
+//Function to prevent User from entering invalid college names in the text box.
+collegeTextInput.addEventListener("blur", (event) => {
+    console.log("input text");
+    const enteredValue = event.target.value;
+    // Check if the entered value matches any of the options
+    if (!collegeData.includes(enteredValue)) {
+        // Clear the input field if the entered value is not a valid option
+        collegeTextInput.value = "";
     }
-  };
-  reader.readAsText(file);
-};
-
-
-async function* makeTextFileLineIterator(fileURL) {
-  const utf8Decoder = new TextDecoder("utf-8");
-  let response = await fetch(fileURL);
-  let file = File(fileURL)
-  let reader = response.body.getReader();
-  let { value: chunk, done: readerDone } = await reader.read();
-  chunk = chunk ? utf8Decoder.decode(chunk, { stream: true }) : "";
-
-  let re = /\r\n|\n|\r/gm;
-  let startIndex = 0;
-
-  for (;;) {
-    let result = re.exec(chunk);
-    if (!result) {
-      if (readerDone) {
-        break;
-      }
-      let remainder = chunk.substr(startIndex);
-      ({ value: chunk, done: readerDone } = await reader.read());
-      chunk =
-        remainder + (chunk ? utf8Decoder.decode(chunk, { stream: true }) : "");
-      startIndex = re.lastIndex = 0;
-      continue;
-    }
-    yield chunk.substring(startIndex, result.index);
-    startIndex = re.lastIndex;
-  }
-  if (startIndex < chunk.length) {
-    // last line didn't end in a newline char
-    yield chunk.substr(startIndex);
-  }
-}
-
-for await (let line of makeTextFileLineIterator(urlOfFile)) {
-  console.log(line);
-}
+});
