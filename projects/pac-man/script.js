1a890a4ee0f898849e3cf15c44a6e86878d62455
Quinn-3 <lin.angelina2169@gmail.com>
2024-01-11 09:39:55 -0500

    added backgrounz

--- projects/pac-man/script.js
+++ projects/pac-man/script.js
@@ -5,5 +5,9 @@ var ctx;
 
 //runs on page load
 window.onload = function(){
-    canvas = document.getElementById("myGame")
+    canvas = document.getElementById("game-canvas");
+    ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,1280,720);
 }
