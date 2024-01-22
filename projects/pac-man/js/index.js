e2cf72988af6b55947fb94da0d60384c18408997
nstaada <staadan25@warrenhills.org>
2024-01-22 08:23:10 -0500

    almost added movement

--- projects/pac-man/js/index.js
+++ projects/pac-man/js/index.js
@@ -2,6 +2,11 @@
 var canvas;
 var ctx;
 
+var upKey;
+var rightKey;
+var downKey;
+var leftKey;
+
 //Create game variables 
 var gameLoop;
 var player;
@@ -12,6 +17,9 @@ window.onload = function(){
     canvas = document.getElementById("game-canvas");
     ctx = canvas.getContext("2d");
 
+
+    setupInputs();
+
     player = new Player(100,400);
     
     gameLoop = setInterval(step, 1000/30)
@@ -21,5 +29,41 @@ window.onload = function(){
 }
 
 function step(){
-    console.log("step");
+    player.step();
+    draw();
+}
+
+function draw(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,1280,720);
+
+    player.draw();
+
+}
+
+function setupInputs(){
+    document.addEventListener("keydown", function(event){
+        if (event.key === "w" || event.key === "ArrowUp"){
+            upKey = true;
+        } else if (event.key === "a" || event.key === "ArrowLeft"){
+            leftKey = true;
+        }else if (event.key === "s" || event.key === "ArrowDown"){
+            downKey = true;
+        }else if (event.key === "d" || event.key === "ArrowRight"){
+            rightKey = true;
+        }
+        
+    });
+    document.addEventListener("keyup", function(event){
+        if (event.key === "w" || event.key === "ArrowUp"){
+            upKey = false;
+        } else if (event.key === "a" || event.key === "ArrowLeft"){
+            leftKey = false;
+        }else if (event.key === "s" || event.key === "ArrowDown"){
+            downKey = false;
+        }else if (event.key === "d" || event.key === "ArrowRight"){
+            rightKey = false;
+        }
+        
+    });
 }
--- projects/pac-man/js/player.js
+++ projects/pac-man/js/player.js
@@ -1,14 +1,22 @@
 function Player(x,y){
     this.x = x;
     this.y = y;
+    this.xspeed = 0;
+    this.yspeed = 0;
+    this.friction = 0.6;
+    this.maxSpeed = 10;
     this.width = 50;
     this.height = 100; 
+    
 
     this.step = function(){
+
 
     }
 
     this.draw = function(){
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x,this.y,this.width,this.height);
 
     }
 
