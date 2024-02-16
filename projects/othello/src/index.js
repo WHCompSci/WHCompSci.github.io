865ee6ae3c2e7820af97fb628411d33343db02b0
nstaada <staadan25@warrenhills.org>
2024-02-16 08:26:14 -0500

    fixed blahblah

---
+++ projects/othello/src/index.js
@@ -0,0 +1,12 @@
+var canvas
+var ctx
+console.log("runningjavcas")
+window.onload = function () {
+    console.log("runningjavcas")
+    canvas = document.getElementById("game-canvas")
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    ctx = canvas.getContext("2d")
+    ctx.font = "100px Arial"
+    ctx.fillText("Othello Game",0,0)
+}
