f1f4c4485e8fbfd2ed236d14932d719dc4788dc8
nstaada <staadan25@warrenhills.org>
2024-02-23 09:14:48 -0500

    sca

--- projects/othello/src/index.js
+++ projects/othello/src/index.js
@@ -1,3 +1,15 @@
+// board constants 
+const sideLen = 40;
+const tileR = sideLen * .4;
+const offSetX = window.innerWidth / 2 - 4 * sideLen;
+const offSetY = window.innerHeight / 2 - 4 * sideLen;
+
+
+const minX = offSetX - sideLen * 0.5;
+const minY = offSetY - sideLen * 0.5;
+const maxX = 7.52 * sideLen + offSetX;
+const maxY = 7.51 * sideLen + offSetY;
+
 var canvas;
 var ctx;
 const board = Array.from({ length: 8 }, _ => Array(8).fill(1));
@@ -15,32 +27,35 @@ window.onload = () => {
     const gameLoop = setInterval(update, 1000 / 30);
 };
 
+addEventListener("click", (ev) => {
+    const x = ev.clientX;
+    const y = ev.clientY; 
+    if( x <minX || y< minY || x> maxX || y>maxY)
+    {
+        return;
+    }
+    console.log("inside");
+    console.log(Math.floor((x-minX)/sideLen));
+    console.log(Math.floor((y-minY)/sideLen));
+
+
+});
 
 
 function update() {
     
     drawBoard(board, ctx);
+    
 }
 
 function drawBoard(board, context) {
-    const sideLen = 50;
-    const tileR = sideLen * .4;
-    const offSetX = canvas.width / 2 - 4 * sideLen;
-    const offSetY = canvas.height / 2 - 4 * sideLen;
-    context.fillStyle = "rgba(255, 0, 0, 0.0)";
-
-    const minX = offSetX - sideLen * 0.5;
-    const minY = offSetY - sideLen * 0.5;
-    const maxX = 7.52 * sideLen + offSetX;
-    const maxY = 7.51 * sideLen + offSetY;
-
-    ctx.fillStyle = "brown";
-    ctx.fillRect(minX - sideLen / 4, minY - sideLen / 4, sideLen * 8 + sideLen / 2, sideLen * 8 + sideLen / 2);
+    context.fillStyle = "brown";
+    context.fillRect(minX - sideLen / 4, minY - sideLen / 4, sideLen * 8 + sideLen / 2, sideLen * 8 + sideLen / 2);
     //.ctx.fillRect(minX-(sideLen/4),minY-(sideLen/4),(sideLen*8),(sidelen*8))
 
 
-    ctx.fillStyle = "green";
-    ctx.fillRect(minX, minY, sideLen * 8, sideLen * 8);
+    context.fillStyle = "green";
+    context.fillRect(minX, minY, sideLen * 8, sideLen * 8);
 
     for (let y = 0; y < 8; y++) {
         for (let x = 0; x < 8; x++) {
@@ -49,7 +64,6 @@ function drawBoard(board, context) {
 
             context.fillStyle = board[y][x] == 1 ? "cornsilk" : "black";
             drawCircle(context, x * sideLen + offSetX, y * sideLen + offSetY, tileR);
-            console.log("draw");
 
         }
     }
