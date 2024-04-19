5a731a1e79b1b4a88e963192224f65b751f8e598
alexsanchez07 <sanchezcoti.15@gamil.com>
2024-04-19 10:08:15 -0400

    push

--- projects/othello-ai/script.js
+++ projects/othello-ai/script.js
@@ -582,3 +582,23 @@ async function minimax_ai(board, legal_moves, is_whites_turn) {
     console.log("Playing Move:", move)
     return move;
 }
+function score_board(board,is_whites_turn)
+{
+    //0000 0000 - black (0)
+    //0000 0001 - white (1)
+
+    //0000 0010
+    let my_chip = is_whites_turn | 2;
+    
+    for( let y = 0; y<8; y++)
+    {
+        for(let x =0; x<8; x++)
+        {
+            let score = board.getCell();
+            if(is_whites_turn)
+            {
+
+            }
+        }
+    }
+}
